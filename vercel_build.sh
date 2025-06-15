368d9bec745bb6cfeff1f4be56307af68241243e
James Hou <james.william.hou@gmail.com>
2025-06-15 17:45:49 -0400

    django1

---
+++ vercel_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Make migrations and migrate
+python manage.py makemigrations
+python manage.py migrate
